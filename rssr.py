456597af49912b0d0923683ff7458eef728cd504
Lucas Messenger <lucas.c.messenger@gmail.com>
2018-12-09 18:49:44 -0800

    Add ability to send email, depend on environment variables, reorganiztion imports

--- rssr.py
+++ rssr.py
@@ -1,25 +1,46 @@
 #!/usr/bin/env python
 
-import feedparser
 from datetime import datetime
-from time import gmtime, mktime
-import json
+from email.message import EmailMessage
+from email.mime.text import MIMEText
+import feedparser
 import html
 from itertools import chain
+import json
+import os
+import smtplib
+from time import gmtime, mktime
 
-INTERVAL_SEC = 21600 # 6 hours
 
 def main():
+
     feeds = [json.loads(line) for line in open('feeds.jsonl')]
     entries = parse_feeds(feeds)
-    template = open('template.html').read()
-    open('email.html', 'w').write(template.format(table=make_table(entries)))
-   # with open('entries.jsonl', 'w') as f:
-   #     for e in entries:
-   #         f.write(json.dumps(e) + '\n')
+    send_email(entries)
+
+
+def send_email(entries):
+
+    if entries:
+
+        template = open('template.html').read()
+
+        msg = EmailMessage()
+        msg.set_content(MIMEText(template.format(table=make_table(entries)), 'html'))
+        msg['Subject'] = '{} feed update(s)'.format(len(entries))
+
+        s = smtplib.SMTP(host=os.environ['EMAIL_HOST'],
+                         port=os.environ['EMAIL_PORT'])
+
+        s.starttls()
+        s.login(user=os.environ['EMAIL_USERNAME'],
+                password=os.environ['EMAIL_PASSWORD'])
+        s.sendmail(os.environ['EMAIL_SENDER'], os.environ['EMAIL_RECIPIENT'], msg.as_string())
+        s.close()
 
 
 def make_table(entries):
+
     output = ''
 
     for e in entries:
@@ -38,20 +59,22 @@ def make_table(entries):
 
 
 def parse_feeds(feeds):
+
     entries = chain.from_iterable([feedparser.parse(f['link']).entries for f in feeds])
     return sorted([process_entry(e) for e in entries
                    if within_interval(e)], key=lambda k: k.get('timestamp'), reverse=True)
 
 
 def process_entry(e):
+
     return {'timestamp': str(datetime.fromtimestamp(mktime(e.published_parsed))),
             'title': e.title,
             'link': e.link}
 
 
 def within_interval(e):
-    return mktime(gmtime()) - mktime(e.published_parsed) <= INTERVAL_SEC
 
+    return mktime(gmtime()) - mktime(e.published_parsed) <= int(os.environ['INTERVAL_SEC'])
 
 
 if __name__ == '__main__':
